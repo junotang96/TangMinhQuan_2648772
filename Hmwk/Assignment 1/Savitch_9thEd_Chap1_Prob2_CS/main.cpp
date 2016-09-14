8afacec417f28a67d2486adc0c6c243fe58035b1
junotang96 <junolais@hotmail.com>
2016-09-13 18:42:30 -0700

    Signed-off-by: junotang96 <junolais@hotmail.com>

---
+++ Hmwk/Assignment 1/Savitch_9thEd_Chap1_Prob2_CS/main.cpp
@@ -0,0 +1,39 @@
+/* 
+   File:   main
+   Author: Minh Quan Tang
+   Created on September 13, 2016, 6:30 PM
+   Purpose: Write a program that prints out "C S !"
+ */
+
+//System Libraries
+#include <iostream>   //Input/Output objects
+using namespace std;  //Name-space used in the System Library
+
+//User Libraries
+
+//Global Constants
+
+//Function prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Declaration of Variables
+   
+    //Input values
+    
+    //Process values -> Map inputs to Outputs
+    
+    //Display Output
+    cout << "   CCC   " << "    SSSS      !!\n";
+    cout << "  C   C  " << "   S    S     !!\n";
+    cout << " C       " <<"   S          !!\n";
+    cout << "C       " << "    S          !!\n";
+    cout << "C       " << "     SSSS      !!\n";
+    cout << "C       " << "         S     !!\n";
+    cout << " C       " << "         S    !!\n";
+    cout << "  C    C " << "  S     S     !!\n";
+    cout << "    CCC   " << "  SSSS       !!\n";
+    cout << "Computer Science is Cool Stuff!!!";
+    //Exit Program
+    return 0;
+}
