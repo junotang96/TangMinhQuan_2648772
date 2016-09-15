d2bebd0bc554a1a7560c8e35ea2aae00bbdcfebb
junotang96 <junolais@hotmail.com>
2016-09-15 16:33:08 -0700

    Signed-off-by: junotang96 <junolais@hotmail.com>

---
+++ Hmwk/Assignment 1/Savitch_9thEd_Chap1_Prob3_CoinCounter/main.cpp
@@ -0,0 +1,32 @@
+/* 
+   File:   main
+   Author: Minh Quan Tang
+   Created on September 13, 2016, 7:10 PM
+   Purpose: 
+ */
+
+//System Libraries
+#include <iostream>   //Input/Output objects
+using namespace std;  //Name-space used in the System Library
+
+//User Libraries
+
+//Global Constants
+
+//Function prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Declaration of Variables
+    int quar, dim, nick, total
+    //Input values
+    cout<<"Enter the number"<<endl;
+    //Process values -> Map inputs to Outputs
+    
+    //Display Output
+
+    //Exit Program
+    return 0;
+}
+
+
