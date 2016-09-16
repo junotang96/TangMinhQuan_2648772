78a60cab995cd16632e798c54a91d82a365b5304
junotang96 <junolais@hotmail.com>
2016-09-15 17:09:27 -0700

    Signed-off-by: junotang96 <junolais@hotmail.com>

--- Hmwk/Assignment 1/Savitch_9thEd_Chap1_Prob3_CoinCounter/main.cpp
+++ Hmwk/Assignment 1/Savitch_9thEd_Chap1_Prob3_CoinCounter/main.cpp
@@ -1,8 +1,8 @@
 /* 
    File:   main
    Author: Minh Quan Tang
-   Created on September 13, 2016, 7:10 PM
-   Purpose: 
+   Created on September 15, 2016, 5:08 PM
+   Purpose: Count the number of coin
  */
 
 //System Libraries
@@ -18,15 +18,15 @@ using namespace std;  //Name-space used in the System Library
 //Execution Begins Here!
 int main(int argc, char** argv) {
     //Declaration of Variables
-    int quar, dim, nick, total
+    int quar=25, dim=10, nick=5, total;
     //Input values
-    cout<<"Enter the number"<<endl;
-    //Process values -> Map inputs to Outputs
-    
-    //Display Output
-
-    //Exit Program
-    return 0;
+   cout<<"Enter the number of quarters, dimes and nickels:\n";cin>>quar>>dim>>nick;
+   
+   total = 25 * quar + 10 * dim + 5 * nick; //total in cents
+   cout<<"The coins are worth ";if (total >= 100) {cout<<total / 100<<" dollar(s) "; }
+   cout<<total % 100<<" cents\n";
+   
+   return 0;
 }
 
 
