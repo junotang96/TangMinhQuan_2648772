d60410de1b48298a068cb5ad627b0125ae692a78
rcc <rcc>
2016-09-26 10:42:04 -0700

    Signed-off-by: rcc <rcc>

--- Lab/GradeIfElse/main.cpp
+++ Lab/GradeIfElse/main.cpp
@@ -1,8 +1,8 @@
 /* 
    File:   main
    Author: Minh Quan Tang
-   Created on September 26th, 2016, 9:44 AM
-   Purpose:  Input a score, output a grade 
+   Created on September 26th, 2016, 9:45 AM
+   Purpose:  Input a score, output a grade using if-else
  */
 
 //System Libraries
@@ -20,20 +20,21 @@ int main(int argc, char** argv) {
     //Declaration of Variables
     char grade;
     unsigned short score;
-   
-    //Input values 
+    
+    //Input values
     cout<<"Input the score, receive your grade as output"<<endl;
     cin>>score;
-  
-    //Process values
-    grade=(score>=90)?'A':
-          (score>=80)?'B':
-          (score>=70)?'C':
-          (score>=60)?'D':'F';
     
-    //Display output
-          cout<<"Your Grade = "<<grade<<" with a score = "<<score<<endl;
-  
+    //Process values -> Map inputs to Outputs
+    if(score>=90)grade='A';
+    else if(score>=80)grade='B';
+    else if(score>=70)grade='C';
+    else if(score>=60)grade='D';
+    else grade='F';
+    
+    //Display Output
+    cout<<"Your Grade = "<<grade<<" with a score = "<<score<<endl;
+
     //Exit Program
     return 0;
 }
