ea5fcae2601ce0c5fdc36af696b2cce30a73c303
rcc <rcc>
2016-10-03 09:36:46 -0700

    Signed-off-by: rcc <rcc>

---
+++ Hmwk/Assignment 3/Savitch_9thEd_Chap3_ProbProj_Prob2/main.cpp
@@ -0,0 +1,51 @@
+/* 
+   File:   main
+   Author: Minh Quan Tang
+   Created on November 3rd, 2016, 8:50 PM
+   Purpose: Algebra Quadratic solution
+ */
+
+//System Libraries
+#include <iostream> 
+#include <cmath>
+//Input/Output objects
+using namespace std;  //Name-space used in the System Library
+
+//User Libraries
+
+//Global Constants
+
+//Function prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Declaration of Variables
+    float a,b,c;
+    
+    //Input values
+    cout<<"Solution to Quadratic ax^2+bx+c=0"<<endl;
+    cout<<"Input 3 float data types a b c"<<endl;
+    cin>>a>>b>>c;
+    
+    //Process values -> Map inputs to Outputs
+    float vb4sqrt=b*b-4*a*c;
+    if(a==0){
+        if(b==0){
+           cout<<"This is not an algebraic equation"<<endl; 
+        }else{
+            cout<<"The solution is x = "<<-c/b<<endl;
+        }
+    }else if(vb4sqrt>0){
+        cout<<"x = ("<<((-b+sqrt(vb4sqrt))/(2*a))<<" , "
+                     <<((-b-sqrt(vb4sqrt))/(2*a))<<")"<<endl;
+             
+    }else{
+        cout<<"x = ("<<(-b/(2*a))<<"+"<<( sqrt(-vb4sqrt)/(2*a))<<"i, "
+                     <<(-b/(2*a))<<"+"<<(-sqrt(-vb4sqrt)/(2*a))<<"i) "<<endl;
+    }
+        
+    //Display Output
+
+    //Exit Program
+    return 0;
+}
