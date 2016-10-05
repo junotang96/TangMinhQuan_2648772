bde5f7e3496a4d8356e321cb02d5366bbc9f0aec
rcc <rcc>
2016-10-05 10:44:06 -0700

    Signed-off-by: rcc <rcc>

---
+++ Lab/SortingProblem/main.cpp
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: rcc
+ *
+ * Created on October 5, 2016, 9:51 AM
+ */
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    int a, b, c, d;
+    a=10;
+    b=5;
+    c=2;
+    if(a>b)
+        cout<<b<<" , "<<a<<endl;
+  
+    else 
+        cout<<a <<b;
+    
+
+    
+    return 0;
+}
+
