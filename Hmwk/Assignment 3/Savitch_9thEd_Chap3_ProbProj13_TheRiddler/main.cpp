11f51016e5b197a0b0f511a36eb5d8edaa79b0c8
rcc <rcc>
2016-10-05 08:27:01 -0700

    Signed-off-by: rcc <rcc>

--- Hmwk/Assignment 3/Savitch_9thEd_Chap3_ProbProj13_TheRiddler/main.cpp
+++ Hmwk/Assignment 3/Savitch_9thEd_Chap3_ProbProj13_TheRiddler/main.cpp
@@ -1,7 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/* 
+   File:   main
+   Author: Minh Quan Tang
+   Created on October 5, 2016, 8:26 PM
+   Purpose: Find the adress of The Riddler
  */
 
 #include <iostream>
